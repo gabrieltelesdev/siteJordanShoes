f1fe9c33de2f468dabe46d76dfb02c7a6f2d2aa9
Gabriel Teles Dev <gabrieltelesdev@hotmail.com>
2022-04-25 20:54:45 -0300

    Adiciona efeito ScrollReveal em JS

---
+++ script.js
@@ -0,0 +1,6 @@
+window.sr = ScrollReveal({ reset: true });
+
+ScrollReveal({ distance: '200px' });
+ScrollReveal().reveal('.texto', { origin: 'left' });
+ScrollReveal().reveal('.destaques', { origin: 'bottom' });
+ScrollReveal().reveal('.nomeLoja', { scale: 1 })
